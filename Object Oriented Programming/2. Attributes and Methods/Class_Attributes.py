68de78002faebdd3bb0c32150bd70d4202add14d
Syed Zain Abbas <96447398+SyedZainAbbas@users.noreply.github.com>
2022-05-08 17:20:36 +0200

    Update Class_Attributes.py

--- Object Oriented Programming/2. Attributes and Methods/Class_Attributes.py
+++ Object Oriented Programming/2. Attributes and Methods/Class_Attributes.py
@@ -1,3 +1,11 @@
+"""
+EXERRCISE: Orange
+Create an Orange class that has one class variable named “count” used to track to track the number of oranges in existence.
+All oranges should have a “weight” instance variable.
+Create 3 different oranges. Make sure your “count” class variable tracks the count correctly.
+Create a separate function (not bound to a class) to add the weight of each orange and get the result.
+"""
+
 class Orange:
     count = 0
     total_weight = 0
